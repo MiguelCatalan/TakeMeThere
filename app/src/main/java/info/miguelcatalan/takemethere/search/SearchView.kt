faeec348bc545c8f78f071c74493ce9def095c37
Miguel Catalan Bañuls <miguel.catalan.banuls@gmail.com>
2017-06-13 18:06:39 +0200

    [search] Implement business logic for the search view

--- app/src/main/java/info/miguelcatalan/takemethere/search/SearchView.kt
+++ app/src/main/java/info/miguelcatalan/takemethere/search/SearchView.kt
@@ -1,5 +1,12 @@
 package info.miguelcatalan.takemethere.search
 
+import com.mapbox.mapboxsdk.geometry.LatLng
 import info.miguelcatalan.takemethere.base.BaseView
 
-interface SearchView : BaseView
+interface SearchView : BaseView {
+    fun centerMapAt(latitude: Double, longitude: Double, zoom: Double)
+    fun clearMap()
+    fun drawPickUp(position: LatLng)
+    fun drawDropOff(position: LatLng)
+    fun showNavigateButton()
+}
